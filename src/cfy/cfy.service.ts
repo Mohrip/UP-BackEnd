52559c9f4497c5d8544e8aa6a040d77d9cce3516
mohrip <m25srt@gmail.com>
2024-10-15 20:18:35 +0300

    "bug fixed in CFY controller"

--- src/cfy/cfy.service.ts
+++ src/cfy/cfy.service.ts
@@ -4,14 +4,14 @@ import { Injectable } from '@nestjs/common';
 export class CfyService {
   calculateEligibility(T: number, Q: number, GPA: number): string {
     if (T > 100 || Q > 100) {
-      return 'Wrong Input!!! The input must be less than or equal to 100';
+      return "Wrong Input!!! The input must be less than or equal to 100";
     }
 
-    const avg = (T * 0.0025) + (Q * 0.0025) + ((GPA / 5) * 50);
-    console.log('thisistheavg', avg);
+    const avg = (T * 0.25) + (Q * 0.25) + ((GPA / 5) * 50);   
+    console.log("thisistheavg", avg);
 
     if (avg >= 95) {
-      return `According to your grades: ( ${`avg`} ), you are eligible to apply for:
+      return `According to your grades: (${avg}), you are eligible to apply for:
         1-Software Engineering
         2-Computer Science
         3-Information System
@@ -22,8 +22,8 @@ export class CfyService {
         8-College of Engineering (General)
         9-Architecture and Building Sciences
         10-Urban Planning`;
-    }  else if (avg >= 90) {
-      return `According to your grades ( ${avg} ), you are eligible to apply for:
+    } else if (avg >= 90) {
+      return `According to your grades: ( ${avg} ), you are eligible to apply for:
         1-Computer Science
         2-Information System
         3-Computer Engineering
@@ -33,13 +33,30 @@ export class CfyService {
         7-College of Engineering (General)
         8-Architecture and Building Sciences
         9-Urban Planning`;
-    } 
-    else {
-      return `According to your grades ( ${ avg } ), you are not eligible to apply for any of the above colleges`;
+    } else {
+      return "According to your grades: ( ${avg} ), you are not eligible to apply for any of the above colleges";
     }
   }
-  // here i want to add a new function that will return the list of saudi colleges that the student can apply to
-  // the function will take the average of the student and return the list of colleges
-    
-  
+
+  // New function to return the list of Saudi colleges based on the average
+  // getSaudiColleges(avg: number): string {
+  //   if (avg >= 95) {
+  //     return `According to your average: ( ${avg} ), you are eligible to apply for:
+  //       1-King Saud University
+  //       2-King Abdulaziz University
+  //       3-King Fahd University of Petroleum and Minerals
+  //       4-Princess Nourah Bint Abdulrahman University
+  //       5-Imam Abdulrahman Bin Faisal University`;
+  //   } else if (avg >= 85) {
+  //     return `According to your average: ( ${avg} ), you are eligible to apply for:
+  //       1-King Saud University
+  //       2-King Abdulaziz University
+  //       3-Princess Nourah Bint Abdulrahman University
+  //       4-Imam Abdulrahman Bin Faisal University`;
+  //   } else {
+  //     return `According to your average: ( ${avg} ), you are eligible to apply for:
+  //       1-King Saud University
+  //       2-Princess Nourah Bint Abdulrahman University`;
+  //   }
+  // }
 }
